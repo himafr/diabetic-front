c7d0a5ad2d013f65f33485f0c0c585dbfcb7570c
hima <hemawith4@gmail.com>
2025-02-16 21:20:55 +0200

    medPage

--- src/pages/MedPage.jsx
+++ src/pages/MedPage.jsx
@@ -1,12 +1,6 @@
-import { useEffect } from "react";
-import useMeds from "../hooks/medsHooks";
-import { useParams } from "react-router-dom";
 
 function MedPage() {
-  const {loadMedById}=useMeds()
-  const id=useParams().id
-
-  return <div>med page {id}</div>;
+  return <div>med page </div>;
 }
 
 export default MedPage;
