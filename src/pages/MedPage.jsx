d352674e78cc20b9a0ec17b5acdf3201ddec6114
hima <hemawith4@gmail.com>
2025-02-16 21:17:48 +0200

    medPage

--- src/pages/MedPage.jsx
+++ src/pages/MedPage.jsx
@@ -6,12 +6,6 @@ function MedPage() {
   const {loadMedById}=useMeds()
   const id=useParams().id
 
-  useEffect(()=> async()=>{
-    // get medicines from db and set them in state here.
-    const data=await loadMedById(id)
-    console.log(data)
-
-  },[])
   return <div>med page {id}</div>;
 }
 
