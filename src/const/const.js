6f0b9a7c73eca2be778176609e899f224e982853
hima <hemawith4@gmail.com>
2025-03-07 16:57:20 +0200

    set online url

--- src/const/const.js
+++ src/const/const.js
@@ -2,5 +2,5 @@ const pc_url= "http://localhost:3000/";
 const phone_url="http://192.168.1.5:3000/";
 const online_url="https://grizzled-abundant-ozraraptor.glitch.me/";
 const online_url2="https://diabeticapi.vercel.app/"
-const baseUrl=pc_url;
+const baseUrl=online_url2;
 export default baseUrl
