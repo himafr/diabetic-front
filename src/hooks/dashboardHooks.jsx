6f9a603e23cc254366bd826b24beee7270af21a7
hima <hemawith4@gmail.com>
2025-04-20 22:55:34 +0200

    done for now

---
+++ src/hooks/dashboardHooks.jsx
@@ -0,0 +1,109 @@
+import {  useState } from "react";
+import axios from "axios";
+import baseUrl from "../const/const";
+import { useAuth } from "../context/AuthContext";
+
+export default function useDashboard() {
+    const {user}=useAuth()
+  const [isLoading, setIsLoading] = useState(true);
+  const [myPatient, setMyPatient] = useState([]);
+  const [freePatient, setFreePatient] = useState([]);
+  const [patientId, setPatientId] = useState(0);
+  const [bgm2, setBgm2] = useState([]);
+  const [bgm,setBgm] = useState([]);
+  const [bgm3, setBgm3] = useState([]);
+    const [myMed,setMyMed] = useState(0);
+    const [otherMed,setOtherMed] = useState(0);
+  
+  async function loadPatientData(value) {
+    try {
+      const res = await axios.get(`${baseUrl}api/v1/users/patient/${value}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.jwt_token}`,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (res?.data?.status === "success" && res.data.data) {
+        const { bgm2 = [], bgm3 = [] } = res.data.data;
+        setBgm2(bgm2.map((e) => e.bgm_num));
+        setBgm3(bgm3.map((e) => e.bgm_num));
+      } else {
+        console.error("Unexpected response format:", res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching patient data:", error);
+    }
+  }
+  
+  async function loadUserData() {
+    setIsLoading(true);
+    const res = await axios.get(`${baseUrl}api/v1/users/doctor_new_patient`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.jwt_token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const { data } = res.data;
+    if (res.data.status === "success") {
+      console.log(data);
+      setMyPatient(data.myPatient);
+      setFreePatient(data.freePatient);
+    }
+    setIsLoading(false);
+  }
+
+  async  function loadPatientInfo(){
+    setIsLoading(true);
+    const res =await axios.get(`${baseUrl}api/v1/users/patient/${user.userId}`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.jwt_token}`,
+            "Content-Type": "application/json",
+        },
+    });
+    const {data}=res.data;
+    if(res.data.status==='success'){
+        setBgm(data.bgm);
+        setBgm2(data.bgm2.map(e=>e.bgm_num));
+        setBgm3(data.bgm3.map(e=>e.bgm_num));
+    }
+    setInterval(()=>{
+      setIsLoading(false);
+    },1000);
+    }
+    
+  async  function loadPharmInfo(){
+    setIsLoading(true);
+    const res =await axios.get(`${baseUrl}api/v1/meds/pharm`, {
+        headers: {
+            Authorization: `Bearer ${localStorage.jwt_token}`,
+            "Content-Type": "application/json",
+        },
+    });
+    const {data}=res.data;
+    if(res.data.status==='success'){
+        setMyMed(data.myMed);
+        setOtherMed(data.otherMed);
+        console.log(data)
+    }
+    setIsLoading(false);
+    }
+   
+
+  return {
+    bgm,
+    bgm2,
+    bgm3,
+    myMed,
+    otherMed,
+    loadPatientData,
+    loadUserData,
+    isLoading,
+    myPatient,
+    freePatient,
+    patientId,
+    setPatientId,
+    loadPatientInfo,
+    loadPharmInfo
+  };
+}
--- src/pages/dashboard/HomePage.jsx
+++ src/pages/dashboard/HomePage.jsx
@@ -1,17 +1,134 @@
-import { useEffect, useState } from "react";
-import NavBar from "../../components/NavBar"
-import PatientDashboard from "./PatientDashboard";
-import DoctorDashboard from "./DoctorDashboard";
-import PharmDashboard from "./PharmDashboard";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState,useCallback } from "react";
+import BarChart from "../../components/BarChart";
+import UserTable from "../../components/UserTable";
+import useDashboard from "../../hooks/dashboardHooks";
+import DoctorTable from "../../components/DoctorTable";
+import PieChart from "../../components/PieChart"
 import { useAuth } from "../../context/AuthContext";
-
 
 // import styles from './HomePage.module.css'
 function HomePage() {
-const {user}=useAuth();
-if (user.role=="patient") return <PatientDashboard />;
-if (user.role=="doctor") return <DoctorDashboard />;
-  if (user.role=="pharmacy") return <PharmDashboard />;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add a listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const { user } = useAuth();
+  const {
+    bgm2,
+    bgm3,
+    loadPatientData,
+    freePatient,
+    myPatient,
+    patientId,
+    setPatientId,
+    isLoading,
+    loadUserData,
+    bgm,
+    loadPatientInfo,
+    loadPharmInfo,
+    myMed,
+    otherMed,
+  } = useDashboard();
+  
+  const setPatientIdValue = useCallback(async (value) => {
+    await loadPatientData(value);
+    setPatientId(value);
+  }, []);
+
+  useEffect(() => {
+    if (user.role == "patient") loadPatientInfo();
+    if (user.role == "doctor") loadUserData();
+    if (user.role == "pharmacy") loadPharmInfo();
+  }, []); // Runs only once when the component mounts
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (user.role == "patient")
+    return (
+      <div>
+        <div className={windowWidth > 700 ? "grid grid-cols-2" : ""}>
+          <BarChart
+            seriesName={["قياسات الشهر الحالي", "قياسات الشهر الماضي"]}
+            seriesValue={[bgm2, bgm3]}
+          />
+          <div> {windowWidth}</div>
+        </div>
+        <UserTable
+          bgm={bgm}
+          header={[
+            "صورة",
+            "اسم المستخدم ",
+            "قياس السكر",
+            "تاريخ القياس",
+            "الحاله",
+          ]}
+        />
+      </div>
+    );
+
+  if (user.role == "doctor")
+    return (
+      <div className="flex flex-wrap">
+        <div className="w-1/2">
+          <p>المرضي المسجلين </p>
+          {myPatient.length !== 0 && (
+            <DoctorTable
+              setPatientId={setPatientIdValue}
+              header={[
+                "صورة",
+                "اسم المستخدم",
+                "الايمال",
+                "تاريخ الانشاء",
+                "الحاله",
+              ]}
+              bgm={myPatient}
+            />
+          )}
+          <p>المرضي الغير مسجلين </p>
+          <DoctorTable
+            setPatientId={setPatientIdValue}
+            header={[
+              "صورة",
+              "اسم المستخدم",
+              "الايمال",
+              "تاريخ الانشاء",
+              "الحاله",
+            ]}
+            bgm={freePatient}
+          />
+        </div>
+        <div className="w-1/2">
+          {patientId && (
+            <BarChart
+              seriesName={["قياسات الشهر الحالي", "قياسات الشهر الماضي"]}
+              seriesValue={[bgm2, bgm3]}
+            />
+          )}
+        </div>
+      </div>
+    );
+  if (user.role == "pharmacy")
+    return (
+      <div>
+        <PieChart
+          labels={["الادوية الخاصه بي", "الادوية الاخري"]}
+          series={[myMed.length, otherMed.length]}
+        />
+      </div>
+    );
 }
 
-export default HomePage
+export default HomePage;
